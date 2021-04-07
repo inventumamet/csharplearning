bfd9873bf753ced5b99cf4dc731aac7e8647bdcc
Kevin Chen <mr.kevin.chen85@gmail.com>
2021-04-07 21:11:14 +1000

    learning info

---
+++ Learning/TetrisSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TetrisSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
