207f0a9a8ab279bf01a2e9ff6529461528180c22
Kevin Chen <mr.kevin.chen85@gmail.com>
2021-04-18 00:10:41 +1000

    added lambda test

---
+++ AWS/LambdaTest/DebuggingExample/src/DebuggingExample/Function.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace DebuggingExample
+{
+    public class Functions
+    {
+        ITimeProcessor processor = new TimeProcessor();
+
+        void LogMessage(ILambdaContext ctx, string msg)
+        {
+            ctx.Logger.LogLine(
+                string.Format("{0}:{1} - {2}", 
+                    ctx.AwsRequestId, 
+                    ctx.FunctionName,
+                    msg));
+        }
+
+
+        private APIGatewayProxyResponse CreateResponse(DateTime? result)
+        {
+            int statusCode = new Func<DateTime?, int>( inputDateTime =>
+            {
+                if (inputDateTime == null)
+                {
+                    return (int)HttpStatusCode.InternalServerError;
+                }
+                else
+                {
+                    return (int)HttpStatusCode.OK;
+                }
+            })(result);
+
+            string body = new Func<DateTime?, string>( inputDateTime =>
+            {
+                if (inputDateTime == null)
+                {
+                    return String.Empty;
+                }
+                else
+                {
+                    return JsonSerializer.Serialize(inputDateTime);
+                }
+            }
+            )(result);
+
+            var response = new APIGatewayProxyResponse
+            {
+                StatusCode = statusCode,
+                Body = body,
+                Headers = new Dictionary<string, string>
+                {
+                    { "Content-Type", "application/json" }, 
+                    { "Access-Control-Allow-Origin", "*" } 
+                }
+            };
+
+            return response;
+        }
+
+        /// <summary>
+        /// Default constructor that Lambda will invoke.
+        /// </summary>
+        public Functions()
+        {
+
+        }
+
+
+        /// <summary>
+        /// A Lambda function to respond to HTTP Get methods from API Gateway
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>The API Gateway response.</returns>
+        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            // context.Logger.LogLine("Get Request\n");
+            LogMessage(context, "Processing request started");
+
+            APIGatewayProxyResponse response;
+            try 
+            {
+                var result = processor.CurrentTimeUTC();
+                response = CreateResponse(result);
+
+                LogMessage(context, "Processing request succeeded.");
+            }
+            catch (Exception ex)
+            {
+                LogMessage(context, string.Format("Processing request failed - {0}", ex.Message));
+                response = CreateResponse(null);
+            }
+
+            return response;
+            // var result = processor.CurrentTimeUTC();
+
+            // return CreateResponse(result);
+
+            // var response = new APIGatewayProxyResponse
+            // {
+            //     StatusCode = (int)HttpStatusCode.OK,
+            //     Body = "Hello AWS Serverless",
+            //     Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+            // };
+
+            // return response;
+        }
+    }
+}
